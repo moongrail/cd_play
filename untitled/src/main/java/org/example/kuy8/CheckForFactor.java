d70c5758fc7f38fbc553f7b04010486ef4ff7e67
moongrail <marselfacegrail@gmail.com>
2026-01-16 15:02:03 +0300

    Revert " =-)"
    This reverts commit bf050128662cc51764ab68fc849d512ed03d4d4a.

--- untitled/src/main/java/org/example/kuy8/CheckForFactor.java
+++
@@ -1,8 +0,0 @@
-package org.example.kuy8;
-
-public class CheckForFactor {
-    public static boolean checkForFactor(int base, int factor) {
-        // your code here
-        return base % factor == 0;
-    }
-}
