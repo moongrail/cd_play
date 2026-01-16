e568b18f977a4c2752b78a62f6f7c5565121a068
moongrail <innomars@yandex.ru>
2026-01-16 15:06:11 +0300

    =-)

--- untitled/src/test/java/org/example/kuy8/CheckForFactorTest.java
+++ untitled/src/test/java/org/example/kuy8/CheckForFactorTest.java
@@ -1,14 +1,15 @@
 package org.example.kuy8;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CheckForFactorTest {
     @Test
     void basicTests() {
-        Assertions.assertTrue(CheckForFactor.checkForFactor(10, 2));
-        Assertions.assertTrue(CheckForFactor.checkForFactor(63, 7));
-        Assertions.assertTrue(CheckForFactor.checkForFactor(2450, 5));
-        Assertions.assertTrue(CheckForFactor.checkForFactor(24612, 3));
+        assertTrue(CheckForFactor.checkForFactor(10, 2));
+        assertTrue(CheckForFactor.checkForFactor(63, 7));
+        assertTrue(CheckForFactor.checkForFactor(2450, 5));
+        assertTrue(CheckForFactor.checkForFactor(24612, 3));
     }
 }
